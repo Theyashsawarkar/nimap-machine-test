a25757c09fab34736b71073b00be2230dba18999
yash sawarkar <yashsawarkar01@gmail.com>
2024-11-16 22:29:21 +0530

    .env added

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,18 +1,22 @@
-import mysql from 'mysql';
+import dotenv from "dotenv";
+import mysql from "mysql";
+
+// Load environment variables from the .env file
+dotenv.config();
 
 const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',         // Replace with your MySQL user
-  password: 'your_password',         // Replace with your MySQL password
-  database: 'machine_test',  // Replace with your DB name
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 db.connect((err) => {
   if (err) {
-    console.error('MySQL connection failed:', err.message);
-    return;
+    console.error("Error connecting to the database: ", err);
+  } else {
+    console.log("Database connected successfully");
   }
-  console.log('Connected to MySQL database.');
 });
 
 export default db;
