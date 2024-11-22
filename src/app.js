b84bef552d0787845c4a7736d7b7fcfc8efcc437
yash sawarkar <yashsawarkar01@gmail.com>
2024-11-22 13:43:35 +0530

    removed cors and body parser

--- src/app.js
+++ src/app.js
@@ -1,20 +1,12 @@
 import express from 'express';
-import bodyParser from 'body-parser';
-import cors from 'cors';
-import categoryRoutes from './routes/category.js';  // Import routes
-import productRoutes from './routes/product.js';    // Import routes
+import categoryRoutes from './routes/category.js'; // Import routes
+import productRoutes from './routes/product.js'; // Import routes
 
 const app = express();
 
 // Middleware
-app.use(bodyParser.json());
-app.use(cors());
-app.use(express.static('src/views'));
-
-// Test route
-app.get('/', (req, res) => {
-  res.send('Welcome to the Node.js Backend!');
-});
+app.use(express.json()); // Parses incoming JSON requests
+app.use(express.static('src/views')); // Serves static files
 
 // API routes
 app.use('/api/categories', categoryRoutes);
